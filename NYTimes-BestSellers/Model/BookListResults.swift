3dd49b71cbe24e42cd4a0ea864dd69d72fcdf9ed
Farhana Mustafa <farhana.swe@gmail.com>
2022-07-22 15:31:31 -0700

    add model
    Signed-off-by: Farhana Mustafa <farhana.swe@gmail.com>

---
+++ NYTimes-BestSellers/Model/BookListResults.swift
@@ -0,0 +1,49 @@
+//
+//  BookListResults.swift
+//  NYTimes-BestSellers
+//
+//  Created by Farhana Mustafa on 7/22/22.
+//
+
+import Foundation
+
+struct BookListResults: Codable {
+    let numResults: Int
+    let results: Results
+
+    enum CodingKeys: String, CodingKey {
+        case numResults = "num_results"
+        case results
+    }
+}
+
+struct Results: Codable {
+    let books: [Book]
+    let listName: String
+    let listNameEncoded: String
+    
+    enum CodingKeys: String, CodingKey {
+        case books
+        case listName = "list_name"
+        case listNameEncoded = "list_name_encoded"
+    }
+}
+
+struct Book: Codable {
+    let author: String
+    let bookImage: String
+    let description: String
+    let title: String
+    let bookImageHeight: Int
+    let bookImageWidth: Int
+           
+    enum CodingKeys: String, CodingKey {
+        case author
+        case description
+        case title
+
+        case bookImage = "book_image"
+        case bookImageHeight = "book_image_height"
+        case bookImageWidth = "book_image_width"
+    }
+}
