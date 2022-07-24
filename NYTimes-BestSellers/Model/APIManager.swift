02fa24bd3d2185481697fb8a6a623c7793f79059
Farhana <farhana.swe@gmail.com>
2022-07-23 19:16:15 -0700

    add config file and apimanager

--- NYTimes-BestSellers/Model/APIManager.swift
+++ NYTimes-BestSellers/Model/APIManager.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+struct APIManager {
+    static let jsonDecoder: JSONDecoder = {
+        return JSONDecoder()
+    }()
+    
+    func fetchBookListResults(with category: String,
+                              successHandler: @escaping (BookListResults) -> Void,
+                              errorHandler: @escaping (Error) -> Void) {
+        let baseURL = "https://api.nytimes.com/svc/books/v3/lists/current/"
+        let apiKey = Config.apiKey
+        let fullURLString = baseURL + category + ".json?api-key=" + apiKey
+        
+        guard let url = URL(string: fullURLString) else {
+            print("Couldn't retrieve a URL from \(fullURLString)")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard error == nil else {
+                print("Error encounted:", error?.localizedDescription ?? "")
+                return
+            }
+            
+            guard let data = data else {
+                print("Something went wrong with data")
+                return
+            }
+            
+            do {
+                let bookListResults = try APIManager.jsonDecoder.decode(BookListResults.self,
+                                                                        from: data)
+                successHandler(bookListResults)
+            } catch {
+                print("Response:", response!)
+                print(error)
+            }
+        }
+        task.resume()
+    }
+}
